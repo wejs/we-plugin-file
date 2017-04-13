9da2fffc8deec6671b7c6aec4cd687fc48d59977
albertosouza <alberto.souza.99@gmail.com>
2017-04-13 20:49:38 -0300

    fix get user avatar url

--- server/controllers/avatar.js
+++ server/controllers/avatar.js
@@ -8,19 +8,20 @@ const fs = require('fs');
 
 module.exports = {
   /**
-   * Get user avatar with user id
+   * Get user avatar with user id, redirect to avatar url if exists or send the default user avatar
    *
    */
   getAvatar(req, res) {
     const we = req.we,
       id = req.params.id;
 
-    let style = req.params.style;
+    const avaibleImageStyles = we.config.upload.image.avaibleStyles;
 
-    if (style === 'responsive') {
-      style = 'large';
-    } else if (!style) {
-      style = 'thumbnail';
+    let imageStyle = req.params.style;
+    if (!imageStyle) {
+      imageStyle = 'original'
+    } else if (imageStyle !== 'original' && avaibleImageStyles.indexOf(imageStyle) === -1) {
+      return res.badRequest('image.style.invalid');
     }
 
     const defaultAvatarPath = we.config.defaultUserAvatar;
@@ -32,27 +33,9 @@ module.exports = {
     .then( (user)=> {
       if (user && user.avatar && user.avatar[0]) {
         // if user have avatar
-        let image = user.avatar[0]
-
-        we.db.models.image.getFileStreamOrResize(image.name, style, (err, stream)=> {
-          if (err) {
-            we.log.error('Error on get avatar: ', err);
-            return res.serverError();
-          }
-          if (!stream) return res.notFound();
-
-          if (image.mime) {
-            res.contentType(image.mime);
-          } else {
-            res.contentType('image/png');
-          }
-
-          stream.pipe(res);
-
-          stream.on('error', function (err) {
-            we.log.error('image:findOne: error in send file', err);
-          })
-        })
+        let image = user.avatar[0];
+        // redirect to avatar image if exists:
+        res.goTo(image.urls[imageStyle]);
       } else {
         // else send the default
         fs.readFile(defaultAvatarPath, (err, contents)=> {
@@ -62,13 +45,13 @@ module.exports = {
         });
       }
 
-      return null
+      return null;
     })
   },
 
   changeAvatar(req, res) {
-    if (!req.isAuthenticated()) return res.forbidden()
-    if (!res.locals.data) return res.notFound()
+    if (!req.isAuthenticated()) return res.forbidden();
+    if (!res.locals.data) return res.notFound();
 
     const we = req.we,
       imageId = req.body.image;
